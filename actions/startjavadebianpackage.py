20d7a4e7964e2acd8056abd647ca4020c9367e8b
Ritz <ryan.ritz@pearson.com>
2017-05-31 07:48:28 -0600

    Fix Exception

--- actions/startjavadebianpackage.py
+++ actions/startjavadebianpackage.py
@@ -39,7 +39,8 @@ class StartJavaDebianPackageAction(Action):
             try:
                 ssh.connect(instanceip, username = 'ubuntu', key_filename=key_file_name)
                 connected = True
-            except (paramiko.SSHException):
+            except (paramiko.SSHException, socket.error) as e:
+                print(e)
                 time.sleep(10)
                 i += 1
         print "Connected to server."
